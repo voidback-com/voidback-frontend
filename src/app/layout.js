4840705b21b565b07d051d7bafc6bcd9eddce577
Mohamed Tah <greyhathacker092@gmail.com>
2025-05-26 12:33:44 +0400

    added google analytics #154

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Roboto, Playfair_Display } from 'next/font/google';
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
+import { useEffect } from "react";
 
 
 export const PlayFair = Playfair_Display({
@@ -29,6 +30,18 @@ export const RobotoFont = Roboto({
 export default function RootLayout({ children }) {
 
 
+  useEffect(()=>{
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+
+    gtag('config', 'G-Y4L9E0R003');
+
+  }, [])
+
+
   return (
 
       <html 
@@ -37,6 +50,10 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
     >
         <head>
+
+        {/** <!-- Google tag (gtag.js) --> **/}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4L9E0R003"></script>
+
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
